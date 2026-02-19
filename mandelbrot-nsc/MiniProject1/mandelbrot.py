5cfd57a18dcac973ef62a4d38ac7bcbd8221d1b5
Simon Bræck Christensen <Simon.braeck@hotmail.com>
2026-02-19 10:36:16 +0100

    Implemented the function mandelbrot_point and created a test to check correct funtionality

--- mandelbrot-nsc/MiniProject1/mandelbrot.py
+++ mandelbrot-nsc/MiniProject1/mandelbrot.py
@@ -1,19 +1,24 @@
 """
 Mandelbrot Set Generator
-Author : [ Your Name ]
+Author : [ Simon Bræck Christensen ]
 Course : Numerical Scientific Computing 2026
 """
-def f(x):
-"""
-Example function .
-Parameters
-----------
-x : float
-Input value
-Returns
--------
-float
-Output value
-"""
+def mandelbrot_point(c, max_iter):
+    z = 0
 
-### TODO: Implement the algorithm
+    for i in range(max_iter):
+        z = z**2+c
+        if abs(z) > 2:
+            return i
+    return max_iter
+
+def mandelbrot_point_test():
+    assert mandelbrot_point(0+0j, 100) == 100
+    assert mandelbrot_point(-1+0j, 100) == 100
+    assert mandelbrot_point(1+1j, 100) < 10
+    assert mandelbrot_point(-0.75+0.1j, 1000) > 10
+
+
+if __name__ == "__main__":   
+    mandelbrot_point_test()
+    print("All Mandelbrot tests passed")
